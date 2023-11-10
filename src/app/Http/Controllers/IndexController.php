e7e54235aa11055a635c2d001c64e962e526055c
JonyTask <yuusu.hyb@gmail.com>
2023-11-11 02:23:22 +0900

    データベース接続回数確認

--- src/app/Http/Controllers/IndexController.php
+++ src/app/Http/Controllers/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\ContactRequest;
 use App\Models\Contact;
+use Illuminate\Support\Facades\DB;
 
 
 class IndexController extends Controller
